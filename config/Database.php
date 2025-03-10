ead4efa92dfba8b96318905615ca8b49a24f7e96
Daniel Sones <djsones@mail.fhsu.edu>
2025-03-10 14:37:51 -0700

    .

--- config/Database.php
+++ config/Database.php
@@ -2,44 +2,36 @@
 
 class Database {
   // DB Params (use environment variables)
+  private $conn;
   private $host;
-  private $db_name;
+  private $port;
+  private $dbname;
   private $username;
   private $password;
-  private $conn;
+
+  public function __construct(){
+    $this->username = getenv('USERNAME');
+    $this->passoword = getenv('PASSWORD');
+    $this->db_name = getenv('DBNAME');
+    $this->host = getenv('HOST');
+    $this->port = getenv('PORT');
+  }
 
   // DB Connect
   public function connect() {
-    $this->conn = null;
+    if ($this->conn){
+      return $this->conn;
+    } else {
 
-    // Load environment variables
-    $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-    $dotenv->load();
+      $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}";
 
-    // Get environment variables
-    $this->host = getenv('DB_HOST');  // Set DB host from environment variable
-    $this->db_name = getenv('DB_NAME');  // Set DB name from environment variable
-    $this->username = getenv('DB_USER');  // Set DB user from environment variable
-    $this->password = getenv('DB_PASS');  // Set DB password from environment variable
-
-    // Check if all the necessary environment variables are set
-    if (!$this->host || !$this->db_name || !$this->username || !$this->password) {
-      // In production, you may want to log this message to a file instead of echoing
-      echo json_encode(array('error' => 'Database connection credentials are missing.'));
-      exit();
+      try {
+        $this->conn = new PDO($dsn, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $this->conn;
+      } catch(PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
+      }
     }
-
-    try { 
-      // Create PDO connection string
-      $dsn = 'pgsql:host=' . $this->host . ';dbname=' . $this->db_name;
-      $this->conn = new PDO($dsn, $this->username, $this->password);
-      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) {
-      echo 'Connection Error: ' . $e->getMessage();
-      echo json_encode(array('error' => 'Database connection failed.'));
-      exit();
-    }
-
-    return $this->conn;
   }
 }
