0141cb34483d63f8bf01eb735ecf92d87b05e6bf
Daniel Sones <djsones@mail.fhsu.edu>
2025-03-17 17:57:16 -0700

    .

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -22,18 +22,9 @@
     $data = json_decode(file_get_contents("php://input"));
 
     // Check for missing parameters and provide specific error messages
-    if (!isset($data->quote)) {
+    if (!isset($data->quote) || !isset($data->author_id) || !isset($data->category_id)) {
+        // If any of the parameters is missing, return a general "Missing Required Parameters" message
         echo json_encode(array('message' => 'Missing Required Parameters'));
-        exit();
-    }
-
-    if (!isset($data->author_id)) {
-        echo json_encode(array('message' => 'author_id Not Found'));
-        exit();
-    }
-
-    if (!isset($data->category_id)) {
-        echo json_encode(array('message' => 'category_id Not Found'));
         exit();
     }
 
